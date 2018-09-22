15986cb260025430148faea8535a1bf1f341d200
Adam Urbina <adamurbina@gmail.com>
2018-09-22 14:34:47 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,11 +1,10 @@
 class SchoolClassesController < ApplicationController
 
   def new
-
   end
 
   def create
-
+    @class = SchoolClass.new(school_params(:title, :room_number))
   end
 
   def show
